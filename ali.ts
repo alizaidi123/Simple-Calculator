6efe95530552963afc395ff125e547da0141e07a
alizaidi123 <159936656+alizaidi123@users.noreply.github.com>
2024-06-22 01:20:05 +0500

    Add files via upload

---
+++ ali.ts
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+const answer: {
+    num1: number
+    num2: number
+    operator: string
+} = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Please Enter The First Number",    
+    },
+
+    {
+        type: "number",
+        name: "num2",
+        message: "Please Enter The Second Number",    
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: [ "Add", "Sub", "Mult", "Div"],
+        message: "Please Select The Operator",    
+    }
+])
+ const { num1, num2, operator } = answer
+ if ( num1 && num2 && operator ){
+    let result: number = 0
+    if ( operator === "Add"){ 
+        result = num1 + num2;
+    }
+    else if ( operator === "Sub" ){ 
+        result = num1 - num2;
+    }
+    else if ( operator === "Mult" ){ 
+        result = num1 * num2;
+    }
+    else if ( operator === "Div" ){ 
+        result = num1 / num2;
+    }
+    console.log(" Your Result is ", result);
+ }
+ else {
+    console.log( "Please Enter The Valid Input" );
+ } 
+ 
